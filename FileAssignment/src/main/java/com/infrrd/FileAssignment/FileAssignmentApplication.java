80f259cd885ce986ed49e43895f1919e4f8d3ba8
Navonil7 <navonilghosh6@gmail.com>
2020-09-10 18:44:38 +0530

    first commit

---
+++ FileAssignment/src/main/java/com/infrrd/FileAssignment/FileAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.infrrd.FileAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAssignmentApplication.class, args);
+	}
+
+}
---
+++ FileAssignment/src/test/java/com/infrrd/FileAssignment/FileAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infrrd.FileAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
